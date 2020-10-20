005889e5bbe8059044dbc3768ce8ee2dd863b05b
arifulhaque828 <arifulhaquecse2015@gmail.com>
2020-10-20 10:21:48 +0600

    Include Update Database

--- master/db/company .sql
+++ master/db/company .sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2020 at 03:18 PM
+-- Generation Time: Oct 20, 2020 at 06:20 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -44,7 +44,7 @@ INSERT INTO `blog` (`id`, `date`, `blog_title`, `blog_details`, `avatar`, `poste
 (9, '2020-10-17', 'WELCOME TO RTSOFTBD1', '1Why do compromises when RTSOFTBD can make your website the way you want?\r\nJust sit and relax, we are here to take care your virtual appearance the best way', 'blog_avatar/avatar1.png', 'RTSOFTBD1'),
 (10, '2020-10-15', 'Web Application', 'Awesome applocation made by RTSOFTBD Web Application TEAM.\r\nVisit our website gather information.', 'blog_avatar/avatar2.png', 'shaown'),
 (11, '2020-10-08', 'think positive', 'Awesome applocation made by RTSOFTBD Web Application TEAM.', 'blog_avatar/avatar3.png', 'admin'),
-(12, '2020-10-09', 'Another blog', 'Awesome applocation made by RTSOFTBD Web Application TEAM.', 'blog_avatar/avatar5.png', 'admin');
+(12, '2020-10-12', 'Another blog12', '12 Awesome applocation made by RTSOFTBD Web Application TEAM.', 'blog_avatar/avatar212.png', 'admin12');
 
 -- --------------------------------------------------------
 
@@ -64,9 +64,9 @@ CREATE TABLE `client_img` (
 INSERT INTO `client_img` (`client_id`, `client_image`) VALUES
 (4, 'client_images/1.png'),
 (5, 'client_images/2.png'),
-(6, 'client_images/3.png'),
+(6, 'client_images/amex1.png'),
 (7, 'client_images/4.png'),
-(8, 'client_images/bkash.png'),
+(8, 'client_images/mastercard.png'),
 (9, 'client_images/rocket.png');
 
 -- --------------------------------------------------------
@@ -89,7 +89,7 @@ CREATE TABLE `copyright` (
 --
 
 INSERT INTO `copyright` (`id`, `year`, `company_name`, `design`, `developed`, `only_on`) VALUES
-(1, '2020', 'RTSOFTBD', 'RTSOFTBD_WEB TEAM', 'RTSOFTBD_APPLICATION TEAM', 'RTSOFTBD');
+(1, '2020-21', 'RTSOFTBD', 'RTSOFTBD_WEB TEAM', 'RTSOFTBD_APPLICATION TEAM', 'RTSOFTBD');
 
 -- --------------------------------------------------------
 
@@ -108,7 +108,7 @@ CREATE TABLE `footer` (
 --
 
 INSERT INTO `footer` (`footer_id`, `footer_heading`, `footer_details`) VALUES
-(6, 'RTSOFTBD Company!!!', 'Qolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibham liber tempor cum soluta nobis eleifend option congue nihil uarta decima et quinta. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut al');
+(6, 'RTSOFTBD Company', 'Qolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibham liber tempor cum soluta nobis eleifend option congue nihil uarta decima et quinta. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut al');
 
 -- --------------------------------------------------------
 
@@ -128,7 +128,7 @@ CREATE TABLE `footer_bottom` (
 --
 
 INSERT INTO `footer_bottom` (`id`, `mobile`, `gmail`, `address`) VALUES
-(4, '01700000', 'rtsoftbd@gmail.com', '132,jahanara garden,dhaka');
+(4, '01700001', 'rtsoftbd@gmail.com', '132,jahanara garden,dhaka');
 
 -- --------------------------------------------------------
 
@@ -146,7 +146,7 @@ CREATE TABLE `footer_img` (
 --
 
 INSERT INTO `footer_img` (`id`, `footer_images`) VALUES
-(13, 'footer_images/20.png'),
+(13, 'footer_images/visa.png'),
 (14, 'footer_images/21.png'),
 (15, 'footer_images/22.png'),
 (16, 'footer_images/23.png'),
@@ -170,12 +170,11 @@ CREATE TABLE `slider` (
 --
 
 INSERT INTO `slider` (`slider_id`, `slider_image`, `slider_descriptions`, `slider_about`) VALUES
-(2, 'slider_images/slider2.png', 'Corporate Solutions', 'Power up the true potential of your business and industry!'),
+(2, 'slider_images/slider2.png', 'Corporate Solution', 'Power up the true potential of your business and industry!'),
 (3, 'slider_images/slider3.png', 'Mobile App & IoT Services', 'Transform your business\r\n										processes with Mobile App & IoT Services.'),
 (4, 'slider_images/slider4.png', 'Digital Marketing', 'We take your business to\r\n										the next level by incorporating a result driven digital marketing strategy with\r\n										a custom designed website and mobile application, tailored for your busin'),
 (5, 'slider_images/slider5.png', 'QA & Test Automation', 'Rest assured on the\r\n										smoothness of functionality, also saving time, money, vastly increasing test\r\n										coverage and software accuracy.\r\n										Team morale improves too!'),
-(6, 'slider_images/slider6_.png', 'web Development', 'Power up the true\r\n										potential of your business and industry!'),
-(10, 'slider_images/slider12.png', 'shaownit solution', 'jdhgasdhasilkud ASKIUJDHFAsiklhjikl asijkhdfLSAJKHFDs asjkhdfliajkSHFDLJKsad jkaSHDLisajhlik asJDBJUKHLas');
+(10, 'slider_images/slider12.png', 'RTSOFTBD', 'jdhgasdhasilkud ASKIUJDHFAsiklhjikl asijkhdfLSAJKHFDs asjkhdfliajkSHFDLJKsad jkaSHDLisajhlik asJDBJUKHLas');
 
 -- --------------------------------------------------------
 
@@ -199,7 +198,9 @@ INSERT INTO `slides_item` (`slides_id`, `slides_title`, `slides_number`) VALUES
 (6, 'Mobile & IOT Service', 3),
 (7, 'Digital Marketing', 4),
 (8, 'QA & Test Automation', 5),
-(9, 'Web Development', 6);
+(9, 'Web Development', 6),
+(88, 'fghdfh', 1),
+(89, 'fghdfh', 1);
 
 -- --------------------------------------------------------
 
@@ -318,37 +319,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `client_img`
 --
 ALTER TABLE `client_img`
-  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `copyright`
 --
 ALTER TABLE `copyright`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `footer`
 --
 ALTER TABLE `footer`
-  MODIFY `footer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `footer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `footer_bottom`
 --
 ALTER TABLE `footer_bottom`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `footer_img`
 --
 ALTER TABLE `footer_img`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `slider`
@@ -360,7 +361,7 @@ ALTER TABLE `slider`
 -- AUTO_INCREMENT for table `slides_item`
 --
 ALTER TABLE `slides_item`
-  MODIFY `slides_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+  MODIFY `slides_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
 -- AUTO_INCREMENT for table `testimonials`
