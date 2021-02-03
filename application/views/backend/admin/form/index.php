a8cef1e0af1d4e6682f10444c9051ff78220e544
arifulhaque828 <arifulhaquecse2015@gmail.com>
2021-02-03 17:09:01 +0600

    project form

--- application/views/backend/admin/form/index.php
+++ application/views/backend/admin/form/index.php
@@ -10,7 +10,7 @@
 		<div class="card card-primary">
 
  <form action="" method="post" enctype="multipart/form-data">
-	<div class="row">
+	<div class="row" style="padding:25px;">
 		<div class="col-md-9"><p>স্মারক নং-</p></div>
 			<div class="col-md-3"><p>তারিখঃ<input type="date"></p>
 			   </div>
@@ -19,11 +19,12 @@
 			<h5 style="text-align:center">সদস্য আবেদন ফরম</h5>
 		   </div>
 		   <div class="col-md-12">
-		   	<h6>
+		   	<h6 style="line-height:28px; ">
 		   		বরাবর</br>
 		        	সভাপতি</br>
 		   	  	শিক্ষিত বেকার কেন্দ্রীয় সঞ্চয় ও ঋণদান সমবায় সমিতি লিঃ</br>
-		   	  	  ওয়ারলেছ বাজার,চাদপুর সদর,চাদপুর </br>
+		   	  	  ওয়ারলেছ বাজার,চাঁদপুর সদর,চাঁদপুর
+ </br>
 		   	</h6>
 		   </div>
 
@@ -33,23 +34,23 @@
                   </h5>
 		   </div>
 		   <div class="col-md-12">
-		   	<h6>
+		   	<h6 style="line-height:28px; ">
 		   		জনাব,</br>
 বিনীত নিবেদন এই যে,আমি নিম্ন স্বাক্ষরকারী আবেদনকৃত সমিতির পক্ষে ক্ষমতাপ্রাপ্ত হয়ে শিক্ষিত বেকার কেন্দ্রীয় সঞ্চয় ও ঋণদান সমবায় সমিতি লিঃ এর সদস্য অন্তর্ভুক্ত হতে বিশেষভাবে আগ্রহী।এই মর্মে শিক্ষিত বেকার কেন্দ্রীয় সঞ্চয় ও ঋণদান সমবায় সমিতি লিঃ এর সকল আইন- কানুনসহ সমিতির উপ-আইন এবং সমবায় সমিতি আইন ও বিধিমালা মেনে চলার পূর্ব অঙ্গীকার ব্যক্ত করছি।
             </h6>
 		   </div>
 		   <div class="col-md-6">
-		   	<h6>
+		   	<h6 style="margin-top:15px; ">
               ১.সমিতির নামঃ
 		    </h6>
 		    <input type="text"  class="form-control"required>
 		   </div>
 		  
-       <div class="col-md-6" >
+       <div class="col-md-6" style="margin-top:15px; ">
 		   <h6>২.সমিতির কার্যালয়ের ঠিকানাঃ </h6>
 		   <textarea  style="background:;" type="text" class="form-control"  required></textarea>
 		    </div>
-      <div class="col-md-6" style="margin-top:10px;">
+      <div class="col-md-6" >
 		   	<h6>
            	    ৩.সমিতির নিবন্ধন নম্বর ও তারিখঃ
 		    </h6>
@@ -61,7 +62,7 @@
 		    </div>
 
 		  
-		     <div class="col-md-6" style="margin-top:10px;">
+		     <div class="col-md-6">
 		   	<h6>
              ৫.সমিতির অনুমোদিত শেয়ার মূলধনঃ 
 		    </h6>
@@ -73,7 +74,7 @@
 		    </h6>
 		    <input type="text"  class="form-control"required>
 		   </div>
-		  <div class="col-md-6" style="margin-top:10px;">
+		  <div class="col-md-6">
 		   	<h6>
          ৭.সমিতির কার্যকরী মূলধনঃ
 		    </h6>
@@ -85,7 +86,7 @@
 		    </h6>
 		    <input type="text"  class="form-control"required>
 		   </div>
-		   <div class="col-md-6" style="margin-top:10px;">
+		   <div class="col-md-6" >
 		   	<h6>
        ৯.সর্বশেষ বার্ষিক সাধারণ সভার তারিখঃ
 		    </h6>
@@ -101,15 +102,15 @@
 		   </div>
 		   
 		 
-		   <div class="col-md-4" style="margin-top:10px;">
+		   <div class="col-md-4" style="margin-top:40px;">
 		   	<p>
 		   		অতএব,উক্ত তথ্য বিবেচনা করে </p>
 
 		   	</div>
-		   	 <div class="col-md-4" style="margin-top:10px;">
+		   	 <div class="col-md-4" style="margin-top:40px;">
 		   	<input type="text"  class="form-control"required>
              </div>
-              <div class="col-md-3" style="margin-top:10px;">
+              <div class="col-md-3" style="margin-top:40px;">
               	<p>
 		   	সমবায় সমিতি লিঃ কে 
 		   	</p>
@@ -119,29 +120,27 @@
 		    শিক্ষিত বেকার কেন্দ্রীয় সঞ্চয় ও ঋণদান সমবায় সমিতি লিঃ এর সদস্য পদে অন্তর্ভুক্তির অনুরোধ করছি।
 		   	</p>
 		   </div>
-		   <div class="col-md-6">
-              	<p>
+		   <div class="col-md-6" style="margin-top:80px;
+		   line-height:28px; ">
+              	     <p>
 		           সংযুক্তিঃ
 		   	           </p>
-		   	           <h6>১.ক্ষমতাপত্র</h6>
-		   	       <h6>২.ব্যবস্থাপনা কমিটির সিদ্ধান্তের রেজুলেশন</h6> 
-                     <h6>৩.নিবন্ধন</h6> 
-                       <h6>৪.ব্যবস্থাপনা কমিটির নির্বাচনী ফলাফল তালিকা</h6> 
-                          <h6>৫.সমিতির অনুমোদিত উপ-আইন</h6>
+		   	        <h6 style="line-height:28px;">১.ক্ষমতাপত্র</br>
+		   	      ২.ব্যবস্থাপনা কমিটির সিদ্ধান্তের রেজুলেশন</br> 
+                    ৩.নিবন্ধন</br> 
+                     ৪.ব্যবস্থাপনা কমিটির নির্বাচনী ফলাফল তালিকা</br> 
+                        ৫.সমিতির অনুমোদিত উপ-আইন</h6>
 		       </div>
-		        <div class="col-md-6">
+		        <div class="col-md-6" style="margin-top:80px;">
               	<p>
 		           
                নিবেদক(সমিতির পক্ষে)
-		   	           </p></br>
-
- 
-
-		   	           <h6> নাম</h6>
+		   	           </p>
+                      <h6> নাম</h6>
 		   	            <input type="text"  class="form-control"required>
-		   	             <h6> পদবী</h6>
+		   	             <h6 style="margin-top:20px;" >  পদবী</h6>
 		   	            <input type="text"  class="form-control"required>
-		   	             <h6> স্বাক্ষর</h6>
+		   	             <h6 style="margin-top:20px;"> স্বাক্ষর</h6>
 		   	            <input type="file"  class="form-control"required>
 		   	      
 		       </div>
@@ -149,7 +148,7 @@
 			        </div>
 
 
-                 <button type="submit" style="margin-left:460px;margin-top:100px;   " class="btn btn-primary">Submit</button></br> 
+                 <button type="submit" style="margin-left:300px;margin-top:100px;   " class="btn btn-primary">Submit</button></br> 
 			</div> 
 
 		 </form>
